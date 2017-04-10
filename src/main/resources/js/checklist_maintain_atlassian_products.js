1c796afed821e12282b2a657aa6496585c91a68a
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2017-04-10 15:26:07 +0300

    fix template and js script

--- src/main/resources/js/checklist_maintain_atlassian_products.js
+++ src/main/resources/js/checklist_maintain_atlassian_products.js
@@ -1,40 +1,37 @@
-console.log("Start js script");
+AJS.toInit(function ($) {
+    function isRequiredField($container, fieldId, error) {
+        var $field = $container.find(fieldId);
 
-function isRequiredField($container, fieldId, error) {
-    var $field = $container.find(fieldId);
-
-    if ($.trim($field.val()) === "") {
-        $field.focus().siblings(".error").html(error);
-        return false;
+        if ($.trim($field.val()) === "") {
+            $field.focus().siblings(".error").html(error);
+            return false;
+        }
+        return true;
     }
-    return true;
-}
 
-function isValidUrl($container, fileId, error) {
-    var $field = $container.find(fieldId);
+    function isValidUrl($container, fieldId, error) {
+        var $field = $container.find(fieldId);
 
-    var url = $field.val();
+        var url = $field.val();
 
-    var re = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
-    if (!re.test(url)) {
-        $field.focus().siblings(".error").html(error);
-        return false;
+        var re = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
+        if (!re.test(url)) {
+            $field.focus().siblings(".error").html(error);
+            return false;
+        }
+        return true;
     }
-    return true;
-}
 
-Confluence.Blueprint.setWizard('net.bytebrand.confluence.plugins.checklist_maintain_atlassian_products.checklist_maintain_atlassian_products:blueprint-item', function (wizard) {
-    wizard.on('submit.wizardMainPage', function (e, state) {
+    Confluence.Blueprint.setWizard('net.bytebrand.confluence.plugins.checklist_maintain_atlassian_products.checklist_maintain_atlassian_products:create-checklist-maintain-atlassian-products', function (wizard) {
+        wizard.on('submit.checklistWizardMainPage', function (e, state) {
+            var $container = state.$container;
+            $container.find(".error").html(""); // clear all existing errors
+            var urlRequired = isRequiredField($container, "#future-url", AJS.I18n.getText("checklist.wizard.blueprint.form.field.url.validation.required"));
+            var customerNameRequired = isRequiredField($container, "#customer-name", AJS.I18n.getText("checklist.wizard.blueprint.form.field.customer.validation.required"));
+            var urlValid = isValidUrl($container, "#future-url", AJS.I18n.getText("checklist.wizard.blueprint.form.field.url.validation.allowedsymbols"));
+            return urlRequired && customerNameRequired && urlValid;
+        });
 
-        console.log(state);
-        var $container = state.$container;
-
-        $container.find(".error").html(""); // clear all existing errors
-
-        var urlRequired = isRequiredField($container, "#future-url", AJS.I18n.getText("checklist.wizard.blueprint.form.field.url.validation.required"));
-        var customerNameRequired = isRequiredField($container, "#customer-name", AJS.I18n.getText("checklist.wizard.blueprint.form.field.customer.validation.required"));
-        var urlValid = isValidUrl($container, "#future-url", AJS.I18n.getText("checklist.wizard.blueprint.form.field.url.validation.required"));
-        return urlRequired && customerNameRequired && urlValid;
     });
-});
 
+});
